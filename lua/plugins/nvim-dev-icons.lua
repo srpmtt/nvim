cdf01df7df913320916d2d6e8f939e7c6bcabc32
srpmtt <m.serpi@outlook.com>
2025-01-06 19:32:26 +0100

    init

---
+++ lua/plugins/nvim-dev-icons.lua
@@ -0,0 +1,18 @@
+return {
+  "nvim-tree/nvim-web-devicons",
+  event = "VeryLazy",
+  config = function()
+    require("nvim-web-devicons").setup {
+      override = {
+        zsh = {
+          icon = "",
+          color = "#428850",
+          cterm_color = "65",
+          name = "Zsh",
+        },
+      },
+      color_icons = true,
+      default = true,
+    }
+  end
+}
