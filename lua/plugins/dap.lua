a1354beaf31606321c219aad143f0556a9a00d25
srpmtt <m.serpi@outlook.com>
2025-03-04 21:03:53 +0100

    init

---
+++ lua/plugins/dap.lua
@@ -0,0 +1,220 @@
+return {
+  "mfussenegger/nvim-dap",
+  event = "VeryLazy",
+  config = function()
+    local dap_config_file = vim.fn.getcwd() .. "/.nvim/dap.lua"
+    local project_dap = nil
+
+    if vim.fn.filereadable(dap_config_file) == 1 then
+      project_dap = dofile(dap_config_file)
+    end
+
+    local dap = require "dap"
+
+    local dap_ui_status_ok, dapui = pcall(require, "dapui")
+    if not dap_ui_status_ok then
+      return
+    end
+
+    dap.listeners.after.event_initialized["dapui_config"] = function()
+      dapui.open()
+    end
+
+    dap.listeners.before.event_terminated["dapui_config"] = function()
+      dapui.close()
+    end
+
+    dap.listeners.before.event_exited["dapui_config"] = function()
+      dapui.close()
+    end
+
+    -- DOTNET
+    dap.adapters.coreclr = project_dap and project_dap.dap.adapters.coreclr
+      or {
+        type = "executable",
+        command = "netcoredbg",
+        args = { "--interpreter=vscode" },
+      }
+
+    dap.configurations.cs = project_dap and project_dap.dap.configurations.cs
+      or {
+        {
+          type = "coreclr",
+          name = "launch",
+          request = "launch",
+          program = function()
+            return vim.fn.input("Path to dll: ", vim.fn.getcwd() .. "/bin/Debug/", "file")
+          end,
+        },
+        {
+          type = "coreclr",
+          name = "attach",
+          request = "attach",
+          processId = require("dap.utils").pick_process,
+        },
+      }
+
+    -- GO
+    dap.adapters.go = {
+      type = "server",
+      port = "${port}",
+      executable = {
+        command = "dlv",
+        args = { "dap", "-l", "127.0.0.1:${port}" },
+      },
+    }
+
+    dap.configurations.go = {
+      {
+        type = "go",
+        name = "launch",
+        request = "launch",
+        program = "${file}",
+      },
+      {
+        type = "go",
+        name = "launch test",
+        request = "launch",
+        mode = "test",
+        program = "${file}",
+      },
+      {
+        type = "go",
+        name = "launch test (go.mod)",
+        request = "launch",
+        mode = "test",
+        program = "./${relativeFileDirname}",
+      },
+    }
+
+    -- JAVASCRIPT
+    dap.adapters["pwa-node"] = {
+      type = "server",
+      host = "localhost",
+      port = "${port}",
+      executable = {
+        command = "node",
+        args = { vim.fn.stdpath "data" .. "/mason/packages/js-debug-adapter/js-debug/src/dapDebugServer.js", "${port}" },
+      },
+    }
+
+    dap.configurations.javascript = {
+      {
+        type = "pwa-node",
+        request = "launch",
+        name = "launch",
+        program = "${file}",
+        cwd = "${workspaceFolder}",
+      },
+    }
+
+    -- PYTHON
+    dap.adapters.python = function(cb, config)
+      if config.request == "attach" then
+        ---@diagnostic disable-next-line: undefined-field
+        local port = (config.connect or config).port
+        ---@diagnostic disable-next-line: undefined-field
+        local host = (config.connect or config).host or "127.0.0.1"
+        cb {
+          type = "server",
+          port = assert(port, "`connect.port` is required for a python `attach` configuration"),
+          host = host,
+          options = {
+            source_filetype = "python",
+          },
+        }
+      else
+        cb {
+          type = "executable",
+          command = vim.fn.stdpath "data" .. "/mason/packages/debugpy/venv/bin/python",
+          args = { "-m", "debugpy.adapter" },
+          options = {
+            source_filetype = "python",
+          },
+        }
+      end
+    end
+
+    dap.configurations.python = {
+      {
+        type = "python",
+        request = "launch",
+        name = "launch",
+        program = "${file}",
+        pythonPath = function()
+          local cwd = vim.fn.getcwd()
+          if vim.fn.executable(cwd .. "/venv/bin/python") == 1 then
+            return cwd .. "/venv/bin/python"
+          elseif vim.fn.executable(cwd .. "/.venv/bin/python") == 1 then
+            return cwd .. "/.venv/bin/python"
+          else
+            return "/usr/bin/python"
+          end
+        end,
+      },
+      {
+        type = "python",
+        request = "launch",
+        name = "launch test",
+        module = "pytest",
+        args = { "${file}" },
+        pythonPath = function()
+          local cwd = vim.fn.getcwd()
+          if vim.fn.executable(cwd .. "/venv/bin/python") == 1 then
+            return cwd .. "/venv/bin/python"
+          elseif vim.fn.executable(cwd .. "/.venv/bin/python") == 1 then
+            return cwd .. "/.venv/bin/python"
+          else
+            return "/usr/bin/python"
+          end
+        end,
+      },
+    }
+
+    -- RUST
+    dap.adapters.codelldb = {
+      type = "server",
+      port = 13000,
+      executable = {
+        command = vim.fn.stdpath "data" .. "/mason/packages/codelldb/codelldb",
+        args = { "--port", "13000" },
+      },
+    }
+
+    dap.configurations.rust = {
+      {
+        name = "launch",
+        type = "codelldb",
+        request = "launch",
+        program = "${workspaceFolder}/target/debug/${workspaceFolderBasename}",
+        cwd = "${workspaceFolder}",
+        stopAtEntry = false,
+        args = {},
+      },
+      {
+        name = "launch test",
+        type = "codelldb",
+        request = "launch",
+        program = "${workspaceFolder}/target/debug/${workspaceFolderBasename}-test",
+        cwd = "${workspaceFolder}",
+        stopAtEntry = false,
+        args = {},
+      },
+      {
+        name = "launch test (specific test)",
+        type = "codelldb",
+        request = "launch",
+        program = "${workspaceFolder}/target/debug/${workspaceFolderBasename}-test",
+        cwd = "${workspaceFolder}",
+        stopAtEntry = false,
+        args = { "--test", "${input:testName}" },
+        input = {
+          testName = {
+            type = "string",
+            description = "Enter the name of the test to run",
+          },
+        },
+      },
+    }
+  end,
+}
