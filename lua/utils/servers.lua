3014226252f586905c8df041d9ab76b1aec120d0
srpmtt <m.serpi@outlook.com>
2025-03-28 12:33:59 +0100

    init

---
+++ lua/utils/servers.lua
@@ -0,0 +1,24 @@
+local servers = {
+  "bashls",
+  "cssls",
+  "denols",
+  "dotls",
+  "dockerls",
+  "hls",
+  "html",
+  "gopls",
+  "jsonls",
+  "lemminx",
+  "lua_ls",
+  "ocamllsp",
+  "omnisharp",
+  "powershell_es",
+  "pylsp",
+  "rust_analyzer",
+  "sqlls",
+  "ts_ls",
+  "yamlls",
+  "zls"
+}
+
+return servers
